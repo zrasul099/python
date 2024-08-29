883265ed65846548b4ab01972738c10ceb34d4aa
zrasul099 <zrasul099@gmail.com>
2024-08-29 16:01:42 -0500

    second day projeect

--- day2.py
+++ day2.py
@@ -13,3 +13,23 @@ print(11+11)
 print(22-11)
 print(22 / 11)
 print(22 * 11)
+
+score = 0
+score += 1
+print(score)
+print("your score is " + str(score))
+height = 1.8
+is_winning = True
+print(f"your score is  {score}" + f"your height is {height}" + f" you are winning {is_winning}" )
+
+total_price  = float(input("How much is total price"))
+tips_persentage = float(input("how much tips would like to give?"))
+tips_amount = (total_price/100)*tips_persentage
+
+people = int(input("how many people are you dividing?"))
+each_person =  (total_price+tips_amount) / people
+
+print(total_price)
+print(tips_amount)
+print(people)
+print(each_person )
