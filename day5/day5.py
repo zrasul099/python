51616972bb18abde3a273148b5c67c88dcc87cbf
zrasul099 <zrasul099@gmail.com>
2024-09-04 13:12:53 -0500

    day 5

--- day5/day5.py
+++ day5/day5.py
@@ -19,8 +19,18 @@ for score in student_score:
         i = score
 print(score)
 
-num = 0
-for number in range(1,20):
-    print(number)
-    num = num+number
-print(num)
+#num = 0
+#for number in range(1,20):
+ #   print(number)
+  #  num = num+number
+#print(num)
+
+for num in range(1,101):
+    if num%3==0:
+        print("Fizz")
+    elif num%5==0:
+        print("buzz")
+    elif num%3==0 and num%5==0:
+        print("FizzBuzz")
+    else:
+        print(num)
