6c29929d8c1ecbb010ed6642017bb3217a4362a4
zrasul099 <zrasul099@gmail.com>
2024-09-14 10:25:09 -0500

    day 8

---
+++ day8/main.py
@@ -0,0 +1,11 @@
+def greet():
+    print("hello")
+    print("hello")
+    print("hello")
+greet()
+
+def greet_with_data(name):
+    print(f"hello {name}")
+    print(f"hello{name}")
+
+greet_with_data("Rasul")
