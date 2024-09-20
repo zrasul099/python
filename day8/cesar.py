ea55c41646e2de1ed40cb1aaa6762468b195957c
zrasul099 <zrasul099@gmail.com>
2024-09-19 22:50:54 -0500

    day 8

---
+++ day8/cesar.py
@@ -0,0 +1,4 @@
+alphabet = ['a','b','c','d','e','f','g','h','k','j','i','l','m','n','o','p','r','s','t','u','v','w','y','z','x','q']
+direction = input("type 'encode' to encrypt, type 'decode' to decrypt:\n" ).lower()
+text = input("Type your message:\n").lower()
+shipt = int(input("Type the shift number:\n"))
