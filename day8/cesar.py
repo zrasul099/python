0dd2c19aa79ad5a0a0ae88c9327f9afc98866a90
zrasul099 <zrasul099@gmail.com>
2024-09-20 22:25:20 -0500

    day 8

--- day8/cesar.py
+++ day8/cesar.py
@@ -2,3 +2,9 @@ alphabet = ['a','b','c','d','e','f','g','h','k','j','i','l','m','n','o','p','r',
 direction = input("type 'encode' to encrypt, type 'decode' to decrypt:\n" ).lower()
 text = input("Type your message:\n").lower()
 shipt = int(input("Type the shift number:\n"))
+
+def encrypt(original_text,shipt_amount ):
+   for letter in original_text:
+       alphabet.index(letter)
+
+print()
