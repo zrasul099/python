924814b6627091d582ecfa7155b811a6a75ce346
zrasul099 <zrasul099@gmail.com>
2024-09-27 22:56:23 -0500

    day 9

---
+++ day9/main.py
@@ -0,0 +1,14 @@
+programming_dictionary = {
+    "Bub": " an error in a program that prevents the program from running as expected",
+    "Funtion": "a piece of code that you can easily call over and over again",
+    "loop": "the action of doing something over and over again "
+}
+#print(programming_dictionary["Bub"])
+#programming_dictionary["Loop"] = "the action of doing something over and over again "
+#print(programming_dictionary)
+#empty_dictionary = {}
+# wipe an existing dictionary
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
